6357e39d37ee11b2e3b5675c714f9b4c4493ce9f
unknown <islam15-3879@diu.edu.bd>
2022-09-04 18:00:26 +0600

    added

--- new1.js
+++ new1.js
@@ -7,8 +7,36 @@ const other =require('./new2')
 const http =require('http')
 
 const server = http.createServer((req,res)=>{
-    res.end('hello node.js')
+    // res.send('hello node.js')
+    // console.log(req.url)
+    // if(req.url=='/'){
+    //     res.writeHead(200,{'Content-Type':'text/html'})
+    //     res.write('<p>This is a home page</p>')
+    //     res.end()
+    // }
+
+    if(req.url=='/'){
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.write(JSON.stringify({course:"ACC"}))
+        res.end()
+    }
+
+
+
+
+    else if(req.url=='/contact'){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write('<p>This is a contact page</p>')
+        res.end()
+    }
+    else if(req.url=='/about-us'){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write('<p>This is a about page</p>')
+        res.end()
+    }
+    
 })
+console.log(url)
 const PORT=5000;
 server.listen(PORT)
 console.log(`SERVER IS RUNNING AT ${PORT}`)
